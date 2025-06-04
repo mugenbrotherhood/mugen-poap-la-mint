48e42e30178646dd7f11171b6c99b39364b9dae6
0xexodia <leonlabrooy215@gmail.com>
2025-06-03 21:40:08 -0400

    Disable problematic Node.js modules instead of polyfilling them - Set zlib, https, http, os to false instead of polyfills - Simplify devDependencies to essential polyfills only - Should resolve twitter-api-v2 and other server-side module conflicts

--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,34 @@
 const webpack = require('webpack');
 
 module.exports = function override(config) {
-  // Add fallbacks for Node.js modules
+  // Add fallbacks for Node.js modules - disable server-only modules
   config.resolve.fallback = {
     ...config.resolve.fallback,
+    // Essential browser polyfills
     "crypto": require.resolve("crypto-browserify"),
-    "stream": require.resolve("stream-browserify"),
+    "stream": require.resolve("stream-browserify"), 
     "buffer": require.resolve("buffer"),
-    "os": require.resolve("os-browserify/browser"),
+    "util": require.resolve("util"),
+    "url": require.resolve("url"),
     "path": require.resolve("path-browserify"),
+    "querystring": require.resolve("querystring-es3"),
+    "assert": require.resolve("assert"),
+    
+    // Disable server-only modules
+    "zlib": false,
+    "https": false,
+    "http": false,
+    "os": false,
     "fs": false,
     "net": false,
     "tls": false,
     "child_process": false,
+    "dns": false,
+    "constants": false,
+    "vm": false,
+    "module": false,
+    "cluster": false,
+    "readline": false,
   };
 
   // Add plugins for polyfills
@@ -24,5 +40,17 @@ module.exports = function override(config) {
     }),
   ];
 
+  // Ignore warnings about missing modules
+  config.ignoreWarnings = [
+    {
+      module: /node_modules/,
+      message: /Critical dependency/,
+    },
+    {
+      module: /node_modules/,
+      message: /Can't resolve/,
+    },
+  ];
+
   return config;
-}; 
+};
